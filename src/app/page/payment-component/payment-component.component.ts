274a7ecbef8cd5abc4aa5474ea1721b5ddfeb0b0
plongrotha <Mrr.rothabetta31@gmail.com>
2025-11-30 16:10:45 +0700

    develop: added admin page

---
+++ src/app/page/payment-component/payment-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-component',
+  imports: [],
+  templateUrl: './payment-component.component.html',
+  styleUrl: './payment-component.component.css'
+})
+export class PaymentComponentComponent {
+
+}
